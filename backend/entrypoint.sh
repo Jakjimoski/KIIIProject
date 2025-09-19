e108adc09bcabf930cc440792dbab82106200a35
Ivica Jakjimoski <ivica.jakjimoski@students.finki.ukim.mk>
2025-09-19 18:25:16 +0200

    script fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "Waiting for database to be ready..."
-#while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
-while ! nc -z "postgres-service" "5432"; do
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+#while ! nc -z "postgres-service" "5432"; do
   sleep 1
 done
 
